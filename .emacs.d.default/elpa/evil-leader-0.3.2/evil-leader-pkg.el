d8db45d0c5250ee79a846799c6fb044736f5047c
Byron Gibson <fbg111@gmail.com>
2013-09-04 10:54:28 -0700

    update

---
+++ .emacs.d.default/elpa/evil-leader-0.3.2/evil-leader-pkg.el
@@ -0,0 +1,1 @@
+(define-package "evil-leader" "0.3.2" "let there be <leader>" (quote ((evil "0"))))
---
+++ submodules-install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+git submodule add git@github.com:repos-emacs/emacs-for-clojure.git submodules/emacs-for-clojure
+
+# New clones of this repo should add these submodules using:
+# http://git-scm.com/book/en/Git-Tools-Submodules
+#git submodule init
+#git submodule update
